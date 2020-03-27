3c734219013ba2719592b25c5d5b0445d3e48fd2
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-03-27 11:21:54 -0300

    Creating separate routes for bootcamps (27/03)

---
+++ routes/bootcamps.js
@@ -0,0 +1,13 @@
+const { Router } = require('express')
+
+const routes = Router()
+
+/*
+routes.get('/')
+routes.get('/:id')
+routes.post('/')
+routes.put('/:id')
+routes.delete('/:id')
+*/
+
+module.exports = routes
--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express')
 const dotenv = require('dotenv')
 const PORT = process.env.PORT || 5000
 
+// Routes files
+const bootcampsRoutes = require('./routes/bootcamps')
+
+// Load env config
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+app.use('/api/v1/bootcamps', bootcampsRoutes)
 
 app.listen(PORT, console.log(`Serve running in ${process.env.NODE_ENV} mode | port ${PORT}`))
