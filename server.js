530a767849824e29548bbea83d4617d92a4a6e93
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-04-25 17:45:57 -0300

    installing Morgan and creating the bootcamps requests (25/04)

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const morgan = require('morgan')
 const PORT = process.env.PORT || 5000
 
 // Routes files
@@ -9,6 +10,11 @@ const bootcampsRoutes = require('./routes/bootcamps')
 dotenv.config({ path: './config/config.env' })
 
 const app = express()
+
+// Dev logging middleware
+if(process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use('/api/v1/bootcamps', bootcampsRoutes)
 
