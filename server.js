3f9df8829e0767fc5bdccbceae8386ceb5b521da
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-04-30 13:22:30 -0300

    Adding MongoDB connection and colors to console (30/04)

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const PORT = process.env.PORT || 5000
+const colors = require('colors')
+const connectDB = require('./config/db')
 
-// Routes files
-const bootcampsRoutes = require('./routes/bootcamps')
+const PORT = process.env.PORT || 5000
 
 // Load env config
 dotenv.config({ path: './config/config.env' })
+
+// Connect to database
+connectDB()
+
+// Routes files
+const bootcampsRoutes = require('./routes/bootcamps')
 
 const app = express()
 
@@ -18,4 +24,14 @@ if(process.env.NODE_ENV == 'development') {
 
 app.use('/api/v1/bootcamps', bootcampsRoutes)
 
-app.listen(PORT, console.log(`Serve running in ${process.env.NODE_ENV} mode | port ${PORT}`))
+const server = app.listen(
+  PORT,
+  console.log(`Serve running in ${process.env.NODE_ENV} mode | port ${PORT}`.yellow.bold)
+)
+
+// Handle unhandled rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red)
+  //Close server & exit process
+  server.close(() => process.exit(1))
+})
