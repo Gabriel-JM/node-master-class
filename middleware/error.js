a920e76b19a05c2300b8b82d61b6c34a267d71dc
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-06 13:46:56 -0300

    Add more error handlings and messages (06/06)

--- middleware/error.js
+++ middleware/error.js
@@ -4,11 +4,22 @@ function errorHandler(err, req, res, next) {
   let error = {...err}
   error.message = err.message
 
-  console.log(err.stack.red)
+  console.log(err)
   
   if(err.name === 'CastError') {
     const message = `Resource not found with id of ${err.value}`
     error = new ErrorResponse(message, 404)
+  }
+
+  const duplicateKey = 11000
+  if(err.code === duplicateKey) {
+    const message = 'Duplicate field value entered'
+    error = new ErrorResponse(message, 400)
+  }
+
+  if(err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message)
+    error = new ErrorResponse(message, 400)
   }
   
   const statusCode = err.statusCode || 500
